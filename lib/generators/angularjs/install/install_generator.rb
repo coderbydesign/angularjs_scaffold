5eb4da11b057700ea3f27a0f68d6f226d197a61b
Keith Walsh <keith.walsh@adtegrity.com>
2013-01-09 11:13:48 -0500

    Tweaking install script

--- lib/generators/angularjs/install/install_generator.rb
+++ lib/generators/angularjs/install/install_generator.rb
@@ -68,7 +68,7 @@ module Angularjs
       @container_class = options["layout-type"] == "fluid" ? "container-fluid" : "container"
       @language = options["language"] == 'javascript' ? "javascript" : "coffeescript"
       @markup = options["markup"] == 'haml' ? 'haml' : 'erb'
-      template "application.html.#{@markup}", "app/views/layouts/application.html.erb"
+      template "application.html.erb", "app/views/layouts/application.html.#{@markup}"
     end
 
     def generate_welcome_controller
